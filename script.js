86e10f5e5ec8dc080cab773fa1e31d85559c49fd
louay beltifa <louay.beltifa1@gmail.com>
2021-07-12 23:30:00 +0100

    Adding Files

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secsEl = document.getElementById("secs");
+
+function countdown(){
+    const newYears = document.getElementById("start").value;
+    
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+    const totalSeconds = ((newYearsDate - currentDate) / 1000 );
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds/ 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor (totalSeconds) % 60;
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
